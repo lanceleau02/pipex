7ef5b5bf9c5a160bd3917065b1b72ea6ebab3b1f
Lancelot Prieure <laprieur@2E5.42angouleme.fr>
2023-03-27 16:53:05 +0200

    git remarche

---
+++ bonus/main.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: laprieur <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 09:48:55 by laprieur          #+#    #+#             */
+/*   Updated: 2023/03/15 14:58:51 by laprieur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <pipex.h>
+
+static void	struct_init(t_pipex *data, char **argv, char **envp)
+{
+	data->pid = 0;
+	data->pipefd[0] = 0;
+	data->pipefd[1] = 0;
+	data->argv2 = argv[2];
+	data->argv3 = argv[3];
+	data->infile = argv[1];
+	data->outfile = argv[4];
+	data->cmd1 = ft_split(argv[2], ' ');
+	data->cmd2 = ft_split(argv[3], ' ');
+	data->envp = envp;
+	data->path = NULL;
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_pipex	data;
+
+	if (argc < 5)
+	{
+		ft_putstr_fd("Error\n", 2);
+		return (1);
+	}
+	struct_init(&data, argv, envp);
+	pipex(&data);
+	waitpid(-1, NULL, 0);
+	waitpid(-1, NULL, 0);
+	ft_free_taboftab(data.cmd1);
+	ft_free_taboftab(data.cmd2);
+	ft_free_taboftab(data.path);
+	return (0);
+}
---
+++ include/pipex_bonus.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: laprieur <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 09:49:21 by laprieur          #+#    #+#             */
+/*   Updated: 2023/03/16 11:29:08 by laprieur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
+
+# include "libft.h"
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <wait.h>
+
+typedef struct s_pipex
+{
+	int		pid;
+	int		pipefd[2];
+	char	*argv2;
+	char	*argv3;
+	char	*infile;
+	char	*outfile;
+	char	**cmd1;
+	char	**cmd2;
+	char	**envp;
+	char	**path;
+}	t_pipex;
+
+int		check_absolute_path(char *cmd);
+int		open_files(char *file, int code);
+int		parsing(char **cmd);
+void	exec_cmd(t_pipex *data, char **cmd);
+void	make_dup2(t_pipex *data, int fd, int code);
+void	pipex(t_pipex *data);
+
+#endif
